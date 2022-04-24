635ebda61e38c5cd0ecfb601ce8dfa900f986d11
maxfelium <maxfelium@users.noreply.github.com>
2022-04-24 07:39:23 +0000

    deploy: 5b21834ea217be28d213f753a4b66c5bee860fc7

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const apiKey = `pk.eyJ1IjoibWF4ZmVsaXUiLCJhIjoiY2wyYmphMHcxMDR4aTNpc2Y5ODVobWNhayJ9.rWHYsp9d_JCrxZGTaEXPew`;
 
-var map = L.map("map").setView([39.9937, -3.0492], 5.7);
+var map = L.map("map").setView([40.5937, -3.0492], 5.7);
 
 L.tileLayer(
   `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}`,
@@ -12,3 +12,17 @@ L.tileLayer(
     accessToken: apiKey,
   }
 ).addTo(map);
+
+// Adding Marker on the map
+
+const markerStJean = L.marker([43.1631, -1.2381]).addTo(map);
+
+// Add popup card
+
+let template = `
+
+<h4>Saint Jean Pied de Port</h4>
+
+`;
+
+markerStJean.bindPopup(template);
