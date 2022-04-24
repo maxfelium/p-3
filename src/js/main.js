b878e2d4ccba225c5d2fbd295945659792a81c8e
MAX FELIU <maxfeliu@ad.unc.edu>
2022-04-24 03:22:30 -0400

    change latitude, longitude, and zoom in js

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,10 @@
 const apiKey = `pk.eyJ1IjoibWF4ZmVsaXUiLCJhIjoiY2wyYmphMHcxMDR4aTNpc2Y5ODVobWNhayJ9.rWHYsp9d_JCrxZGTaEXPew`;
 
-var map = L.map("map").setView([51.505, -0.09], 13);
+var map = L.map("map").setView([39.9937, -3.0492], 5.7);
 
 L.tileLayer(
   `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}`,
   {
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: "mapbox/streets-v11",
     tileSize: 512,
